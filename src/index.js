df5939fa3150e8ad988454578a4b553043492f27
Galina-T <kazarinagg@gmail.com>
2021-04-02 10:27:33 +0300

    build was prepared

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {add} from './js/utils';
+import './style.scss';
+
+const res = add(5, 5);
+
+console.log(res);
+console.log('hello');
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.pug$/i,
+                use: [
+                    "pug-loader",
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/index.pug'
+    })],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        hot: true,
+    },
+};
